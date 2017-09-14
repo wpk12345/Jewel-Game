587b8e4169411b7e19372467fdd4c5d1f391381a
William King <wpk12345@gmail.com>
2017-09-14 18:35:06 -0400

    9-14-17 commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,137 @@
 $(document).ready(function() {
 
-    //Global variables
 
-    //Need to write a "press the play button to begin playing" function
+ $("#instructions").click(function(){
+    
+ });
 
-    // $("#start-button").on("click", function() {
+    $("#start-button").click(function() {
+        $("#start-button, #pushed").hide();
+   
 
-    var startValue = Math.floor(Math.random() * 101) + 19;
-    var wins = 0;
-    var losses = 0;
-    console.log(startValue, wins, losses);
-    var jewel1;
-    var jewel2;
-    var jewel3;
-    var jewel4;
-    $("#targetNum").html(startValue);
-    $("#wins").append(wins);
-    $("#loss").append(losses);
-
-
-    // })
-
-    //Need to have an updated user score
-
-
-
-    //The player will be shown a random # ranging from 19-120at the start of each game
-    //I need to set a random # ranging from 19-120 for each crystal image
-
-    //New game function that resets the image amounts and starting amount
-    function gameReset() {
         var startValue = Math.floor(Math.random() * 101) + 19;
+        var wins = 0;
+        var losses = 0;
+        var counter = 0;
+        console.log(startValue, wins, losses);
+        var img1 = Math.floor(Math.random() * 12) + 1;
+        var img2 = Math.floor(Math.random() * 12) + 1;
+        var img3 = Math.floor(Math.random() * 12) + 1;
+        var img4 = Math.floor(Math.random() * 12) + 1;
+        console.log(img1, img2, img3, img4);
 
-    }
-
-
-    //A function when ran sets Each crystal's hidden value between 1-12
-    function imageReset() {
-        var random = Math.floor(Math.random() * 12) + 1;
-        console.log(random);
-
-    }
-
-
-    //When a crystal is clicked, it will add a specific amount of points to the player's score
-    //The game will hide this amount until the player clicks a crystal
-    //When they do click one, update the player's score counter.
-
-    //The player wins if their total score matches the random # from the beginning
-
-    //The player loses if he goes above the random #
-
-    //The game restarts whenever the player wins or loses.
-
-    //When the game restarts the player should see a new #,
-    //also all 4 crystals will have 4 new hidden values, and 
-    //user's score (and score counter) will reset to 0.
-
-    //The app should tally wins/losses when new game starts
+        //puts the starting # in the html
+        $("#targetNum").html(startValue);
+        //initializes the wins losses and beginning number to 0
+        $("#wins").html(wins);
+        $("#loss").html(losses);
+        $("#totalNum").html(counter);
 
 
+        //each time an image is clicked i'm adding that numbers value to var counter.
+        $("#img1").click(function() {
+            counter += img1;
+            //showing the current number as it's tallied
+            $("#totalNum").html(counter);
+            console.log(counter);
+
+            //if then when the user reaches the starting number or goes over it
+            if (counter === startValue) {
+                wins++;
+                $("#wins").html(wins);
+                console.log("wins: " + wins);
+                reset();
+                $("#totalNum").html(counter);
+
+            } else if (counter >= startValue) {
+                losses++;
+                $("#loss").html(losses);
+                console.log("losses: " + losses);
+                reset();
+                $("#totalNum").html(counter);
+            }
+        })
+
+        //below is basically everything the img1 code above is doing for the remaining 3 images
+        $("#img2").click(function() {
+            counter += img2;
+            $("#totalNum").html(counter);
+            console.log(counter);
+            if (counter === startValue) {
+                wins++;
+                $("#wins").html(wins);
+                console.log("wins: " + wins);
+                reset();
+                $("#totalNum").html(counter);
+
+            } else if (counter >= startValue) {
+                losses++;
+                $("#loss").html(losses);
+                console.log("losses: " + losses);
+                reset();
+                $("#totalNum").html(counter);
+            }
+        })
 
 
+        $("#img3").click(function() {
+            counter += img3;
+            $("#totalNum").html(counter);
+            console.log(counter);
+            if (counter === startValue) {
+                wins++;
+                $("#wins").html(wins);
+                console.log("wins: " + wins);
+                reset();
+                $("#totalNum").html(counter);
+
+            } else if (counter >= startValue) {
+                losses++;
+                $("#loss").html(losses);
+                console.log("losses: " + losses);
+                reset();
+                $("#totalNum").html(counter);
+            }
+        })
+
+
+        $("#img4").click(function() {
+            counter += img4;
+            $("#totalNum").html(counter);
+            console.log(counter);
+            if (counter === startValue) {
+                wins++;
+                $("#wins").html(wins);
+                console.log("wins: " + wins);
+                reset();
+                $("#totalNum").html(counter);
+
+            } else if (counter >= startValue) {
+                losses++;
+                $("#loss").html(losses);
+
+                console.log("losses: " + losses);
+                reset();
+                $("#totalNum").html(counter);
+            }
+        })
+        //reset function that can be called when user wins or losses
+        function reset() {
+            startValue = Math.floor(Math.random() * 101) + 19;
+            console.log("new start value: " + startValue);
+            $("#targetNum").html(startValue);
+            counter = 0;
+            console.log(startValue, wins, losses);
+            img1 = Math.floor(Math.random() * 12) + 1;
+            img2 = Math.floor(Math.random() * 12) + 1;
+            img3 = Math.floor(Math.random() * 12) + 1;
+            img4 = Math.floor(Math.random() * 12) + 1;
+            console.log(img1, img2, img3, img4);
+            //I want to change the "Beginning number to New Number"
+
+
+        }
+    })
 
 
 });
